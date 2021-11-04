1a60f3c6e43590acf299cca35daf98e69a008515
kellerberrin <james.duncan.mcculloch@gmail.com>
2021-11-04 14:29:27 +1100

    Pubmed literature analysis prior to research scoring.

--- kgl_analytic/kgl_verify/kgl_analysis_null.cpp
+++ kgl_analytic/kgl_verify/kgl_analysis_null.cpp
@@ -35,8 +35,8 @@ bool kgl::NullAnalysis::initializeAnalysis(const std::string& work_directory,
   // Get the pubmed api
   pubmed_requestor_ptr_ = resource_ptr->getSingleResource<const PubmedRequester>(RuntimeResourceType::PUBMED_API);
 
-//  std::vector<std::string> pmidids{ "16759385", "19281305", "20401335", "21029472", "21790707", "21867552", "21929748"};
-std::vector<std::string> pmidids{ "22157630", "18717995", "7886766" };
+  std::vector<std::string> pmidids{ "16759385", "19281305", "20401335", "21029472", "21790707", "21867552", "21929748"};
+// std::vector<std::string> pmidids{ "22157630", "18717995", "7886766" };
   auto publication_map = pubmed_requestor_ptr_->getCachedPublications(pmidids);
 
   for (auto const& [pmid, publication_ptr] : publication_map) {
