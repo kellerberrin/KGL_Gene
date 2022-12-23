38a5a34075ec82f02002f6920ea4da9b43d1f3d8
kellerberrin <james.duncan.mcculloch@gmail.com>
2022-12-23 16:17:02 +1100

    Create the multi-threading library.

--- kel_thread/kel_workflow_queue.h
+++ kel_thread/kel_workflow_queue.h
@@ -2,170 +2,37 @@
 // Created by kellerberrin on 14/12/22.
 //
 
-#ifndef KEL_WORKFLOW_QUEUE_H
-#define KEL_WORKFLOW_QUEUE_H
+#ifndef KEL_WORKFLOW_QUEUES_H
+#define KEL_WORKFLOW_QUEUES_H
 
-#include <condition_variable>
 #include <functional>
-#include <iostream>
-#include <future>
 #include <vector>
 #include <thread>
-#include <queue>
-#include <algorithm>
-
 
 #include "kel_mt_queue.h"
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// A threaded workflow for copyable objects, specifically NOT std::unique_ptr. Use std::shared_ptr with this object.
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-namespace kellerberrin {   //  organization level namespace
-
-
-template<typename T, typename U>
-requires (std::copy_constructible<T> && std::equality_comparable<T>) && (std::copy_constructible<U> && std::equality_comparable<U>)
-class WorkflowThreads
-{
-
-  using Proc = std::function<U(T)>;
-
-public:
-
-  WorkflowThreads() = default;
-  ~WorkflowThreads() noexcept {
-
-    stopProcessing();
-
-  }
-
-  // Convenience routines, default is available hardware threads minus 1, minimum 1 thread.
-  [[nodiscard]] static size_t defaultThreads() { return std::max<size_t>(std::thread::hardware_concurrency() - 1, 1); }
-  [[nodiscard]] static size_t defaultThreads(size_t job_size) { return (job_size > 0 ? std::min<size_t>(defaultThreads(), job_size) : 1); }
-
-  // Assumes the function has a void return type, does not return a future.
-  template<typename F, typename... Args>
-  void registerProcessingFn(T input_stop, U output_stop, size_t threads, F f, Args... args) noexcept
-  {
-
-    input_stop_ = input_stop;
-    output_stop_ = output_stop;
-    workflow_callback_ = [f, args...](T t)->U { return f(t, args...); };
-    queueThreads(threads);
-
-  }
-
-
-  void push(T input_obj) {
-
-    input_queue_.push(input_obj);
-
-  }
-
-  [[nodiscard]] U waitAndPop() {
-
-    return output_queue_.waitAndPop();
-
-  }
-
-  void stopProcessing() {
-
-    push(input_stop_);
-
-    for(auto& thread : threads_) {
-
-      thread.join();
-
-    }
-
-    threads_.clear();
-
-  // There are no active threads processing the input queue.
-
-    output_queue_.push(output_stop_);
-
-  }
-
-  [[nodiscard]] size_t threadCount() const { return threads_.size(); }
-  [[nodiscard]] const MtQueue<T>& inputQueue() const { return input_queue_; }
-  [[nodiscard]] const MtQueue<U>& outputQueue() const { return output_queue_; }
-
-private:
-
-  MtQueue<T> input_queue_;
-  T input_stop_;
-  MtQueue<U> output_queue_;
-  U output_stop_;
-  Proc workflow_callback_;
-  std::vector<std::thread> threads_;
-
-  void queueThreads(size_t threads)
-  {
-
-    // Always have at least one worker thread queued.
-    threads = std::max<size_t>(threads, 1);
-
-    // Queue the worker threads,
-    for(size_t i = 0; i < threads; ++i) {
-
-      threads_.emplace_back(&WorkflowThreads::threadProlog, this);
-
-    }
-
-  }
-
-  void threadProlog() {
-
-    while(true) {
-
-      T workItem = input_queue_.waitAndPop();
-
-      if (workItem == input_stop_) {
-
-        push(input_stop_);
-
-        break;
-
-      } else {
-
-        output_queue_.push(workflow_callback_(workItem));
-
-      }
-
-    }
-
-  }
-
-};
-
+namespace kellerberrin {  //  organization level namespace
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// A threaded workflow for std::move constructable objects. Use std::unique_ptr with this object.
+// A threaded workflow for std::move constructable objects (std::unique_ptr).
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 template<typename T, typename U>
 requires (std::move_constructible<T> && std::equality_comparable<T>) && (std::move_constructible<U> && std::equality_comparable<U>)
-class WorkflowMove
+class WorkflowQueues
 {
 
   using Proc = std::function<U(T)>;
 
 public:
 
-  WorkflowMove() = default;
-  ~WorkflowMove() noexcept {
-
-    stopProcessing();
-
-  }
+  WorkflowQueues() = default;
+  ~WorkflowQueues() noexcept { stopProcessing(); }
 
   // Convenience routines, default is available hardware threads minus 1, minimum 1 thread.
   [[nodiscard]] static size_t defaultThreads() { return std::max<size_t>(std::thread::hardware_concurrency() - 1, 1); }
@@ -178,20 +45,13 @@ public:
 
     input_stop_ = std::move(input_stop);
     output_stop_ = std::move(output_stop);
-    workflow_callback_ = [f, args...](T&& t)->U { return f(std::move(t), args...); };
+    workflow_callback_ = [f, args...](T&& t)->U { return f(args..., std::move(t)); };
     queueThreads(threads);
 
   }
 
-
   // Input stop tokens are rejected.
   void push(T&& input_obj) {
-
-    if (input_obj == input_stop_) {
-
-      return;
-
-    }
 
     input_queue_.push(std::move(input_obj));
 
@@ -203,33 +63,8 @@ public:
 
   }
 
-  void stopProcessing() {
 
-    // Push the stop token onto the input queue.
-    input_queue_.push(std::move(input_stop_));
-
-    // Join the active threads
-    for(auto& thread : threads_) {
-
-      thread.join();
-
-    }
-
-    // Remove the inactive threads.
-    threads_.clear();
-    // Ensure the input queue is empty
-    while(not input_queue_.empty()) {
-
-      T t = input_queue_.waitAndPop();
-
-    }
-
-    // Push the stop token onto the output queue.
-    output_queue_.push(std::move(output_stop_));
-
-  }
-
-  [[nodiscard]] size_t threadCount() const { return threads_.size(); }
+  // Queue state access routines.
   [[nodiscard]] const MtQueue<T>& inputQueue() const { return input_queue_; }
   [[nodiscard]] const MtQueue<U>& outputQueue() const { return output_queue_; }
 
@@ -240,10 +75,15 @@ private:
   MtQueue<U> output_queue_;
   U output_stop_;
   Proc workflow_callback_;
-  std::vector<std::jthread> threads_;
+  std::vector<std::thread> threads_;
+  std::atomic<uint32_t> active_threads_{0};
+
 
   void queueThreads(size_t threads)
   {
+
+    // Remove any existing threads.
+    stopProcessing();
 
     // Always have at least one worker thread queued.
     threads = std::max<size_t>(threads, 1);
@@ -251,7 +91,8 @@ private:
     // Queue the worker threads,
     for(size_t i = 0; i < threads; ++i) {
 
-      threads_.emplace_back(&WorkflowMove::threadProlog, this);
+      threads_.emplace_back(&WorkflowQueues::threadProlog, this);
+      ++active_threads_;
 
     }
 
@@ -261,17 +102,210 @@ private:
 
     while(true) {
 
-      T workItem = input_queue_.waitAndPop();
+      T work_item = input_queue_.waitAndPop();
 
-      if (workItem == input_stop_) {
+      if (work_item == input_stop_) {
 
-        input_queue_.push(std::move(workItem));
+        // If the last thread then queue a stop token on the output queue.
+        if (--active_threads_ == 0) {
+
+          // Push the stop token onto the output queue.
+          output_queue_.push(std::move(output_stop_));
+
+        } else { // Push a stop token on the input queue to stop another active thread.
+
+          input_queue_.push(std::move(work_item));
+
+        }
         break;
 
       } else {
 
-        U result = std::move(workflow_callback_(std::move(workItem)));
-        output_queue_.push(std::move(result));
+        U u = std::move(workflow_callback_(std::move(work_item)));
+        output_queue_.push(std::move(u));
+
+      }
+
+    }
+
+  }
+
+  void stopProcessing() {
+
+    // If any active threads then push the stop token onto the input queue.
+    if (active_threads_ != 0) {
+
+      input_queue_.push(std::move(input_stop_));
+
+    }
+
+    // Join all the threads
+    for(auto& thread : threads_) {
+
+      thread.join();
+
+    }
+
+    threads_.clear();
+
+  }
+
+};
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// A threaded workflow for std::move constructable objects (std::unique_ptr).
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<typename T>
+requires (std::move_constructible<T> && std::equality_comparable<T>)
+class WorkflowBase
+{
+
+public:
+
+  explicit WorkflowBase(T stop_token) : stop_token_(std::move(stop_token)) {}
+  ~WorkflowBase() { stopProcessing(); };
+
+  // Convenience routines, default is available hardware threads minus 1, minimum 1 thread.
+  [[nodiscard]] static size_t defaultThreads() { return std::max<size_t>(std::thread::hardware_concurrency() - 1, 1); }
+  [[nodiscard]] static size_t defaultThreads(size_t job_size) { return (job_size > 0 ? std::min<size_t>(defaultThreads(), job_size) : 1); }
+
+
+  // Input stop tokens are rejected.
+  virtual void push(T&& input_obj) = 0;
+
+
+//protected:
+
+  MtQueue<T> queue_;
+  T stop_token_;
+  std::vector<std::thread> threads_;
+  std::atomic<uint32_t> active_threads_{0};
+
+  void stopProcessing() {
+
+    // If any active threads then push the stop token onto the input queue.
+    if (active_threads_ != 0) {
+
+      queue_.push(std::move(stop_token_));
+
+    }
+
+    // Join all the threads
+    for(auto& thread : threads_) {
+
+      thread.join();
+
+    }
+
+    threads_.clear();
+
+  }
+
+};
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// A threaded workflow for std::move constructable objects (std::unique_ptr).
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<typename T, typename U>
+requires (std::move_constructible<T> && std::equality_comparable<T>) && (std::move_constructible<U> && std::equality_comparable<U>)
+class WorkflowDouble : public WorkflowBase<T>
+{
+
+  using Proc = std::function<U(T)>;
+
+public:
+
+  WorkflowDouble(T stop_token, std::unique_ptr<WorkflowBase<U>>&& output_queue_ptr) : WorkflowBase<T>(std::move(stop_token)), output_queue_ptr_(std::move(output_queue_ptr))  {}
+  ~WorkflowDouble() = default;
+
+
+  // Note that the variadic args... are presented to ALL active threads and must be thread safe.
+  template<typename F, typename... Args>
+  void registerProcessingFn(size_t threads, F&& f, Args&&... args) noexcept
+  {
+
+    workflow_callback_ = [f, args...](T&& t)->U { return f(args..., std::move(t)); };
+    queueThreads(threads);
+
+  }
+
+  void push(T&& input_obj) final {
+
+    WorkflowBase<T>::queue_.push(std::move(input_obj));
+
+  }
+
+  [[nodiscard]] U waitAndPop() {
+
+    return output_queue_ptr_->queue_.waitAndPop();
+
+  }
+
+
+  // Queue state access routines.
+  [[nodiscard]] const MtQueue<T>& inputQueue() const { return WorkflowBase<T>::queue_; }
+  [[nodiscard]] const MtQueue<U>& outputQueue() const { return output_queue_ptr_->queue_; }
+
+private:
+
+  std::unique_ptr<WorkflowBase<U>> output_queue_ptr_;
+  Proc workflow_callback_;
+
+  void queueThreads(size_t threads)
+  {
+
+    // Remove any existing threads.
+    WorkflowBase<T>::stopProcessing();
+
+    // Always have at least one worker thread queued.
+    threads = threads < 1 ? 1 : threads;
+
+    // Queue the worker threads,
+    for(size_t i = 0; i < threads; ++i) {
+
+      WorkflowBase<T>::threads_.emplace_back(&WorkflowDouble::threadProlog, this);
+      ++WorkflowBase<T>::active_threads_;
+
+    }
+
+  }
+
+  void threadProlog() {
+
+    while(true) {
+
+      T work_item = WorkflowBase<T>::queue_.waitAndPop();
+
+      if (work_item == WorkflowBase<T>::stop_token_) {
+
+        // If the last thread then queue a stop token on the output queue.
+        if (--WorkflowBase<T>::active_threads_ == 0) {
+
+          // Push the stop token onto the output queue.
+          output_queue_ptr_->queue_.push(std::move(output_queue_ptr_->stop_token_));
+
+        } else { // Push a stop token on the input queue to stop another active thread.
+
+          WorkflowBase<T>::queue_.push(std::move(work_item));
+
+        }
+        break;
+
+      } else {
+
+        U u = std::move(workflow_callback_(std::move(work_item)));
+        output_queue_ptr_->queue_.push(std::move(u));
 
       }
 
@@ -282,6 +316,110 @@ private:
 };
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// A threaded workflow for std::move constructable objects (std::unique_ptr).
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+template<typename T>
+requires (std::move_constructible<T> && std::equality_comparable<T>)
+class WorkflowSingle : public WorkflowBase<T>
+{
+
+  using Proc = std::function<void(T)>;
+  using Prolog = void (WorkflowSingle<T>::*)();
+
+public:
+
+  explicit WorkflowSingle(T stop_token) : WorkflowBase<T>(std::move(stop_token)), prolog_proc_(&WorkflowSingle<T>::threadProlog) {}
+  ~WorkflowSingle() = default;
+
+  // Note that the variadic args... are presented to ALL active threads and must be thread safe.
+  template<typename F, typename... Args>
+  void registerProcessingFn(size_t threads, F&& f, Args&&... args) noexcept
+  {
+
+    workflow_callback_ = [f, args...](T&& t)->void { return f(args..., std::move(t)); };
+    queueThreads(threads);
+
+  }
+
+  // Input stop tokens are rejected.
+  void push(T&& input_obj) {
+
+    WorkflowBase<T>::queue_.push(std::move(input_obj));
+
+  }
+
+  [[nodiscard]] T waitAndPop() {
+
+    return WorkflowBase<T>::queue_.waitAndPop();
+
+  }
+
+  // Queue state access routines.
+  [[nodiscard]] const MtQueue<T>& inputQueue() const { return WorkflowBase<T>::queue_; }
+
+private:
+
+  Proc workflow_callback_;
+  Prolog prolog_proc_;
+
+  void queueThreads(size_t threads)
+  {
+
+    // Remove any existing threads.
+    WorkflowBase<T>::stopProcessing();
+
+    // Always have at least one worker thread queued.
+    threads = std::max<size_t>(threads, 1);
+
+    // Queue the worker threads,
+    for(size_t i = 0; i < threads; ++i) {
+
+      WorkflowBase<T>::threads_.emplace_back(prolog_proc_, this);
+      ++WorkflowBase<T>::active_threads_;
+
+    }
+
+  }
+
+  void threadProlog() {
+
+    while(true) {
+
+      T work_item = WorkflowBase<T>::queue_.waitAndPop();
+
+      if (work_item == WorkflowBase<T>::stop_token_) {
+
+        // If the last thread then do not queue a stop token.
+        if (--WorkflowBase<T>::active_threads_ != 0) {
+
+          WorkflowBase<T>::queue_.push(std::move(work_item));
+
+        } else {
+
+          workflow_callback_(std::move(work_item));
+
+        }
+        break;
+
+      } else {
+
+        workflow_callback_(std::move(work_item));
+
+      }
+
+    }
+
+  }
+
+};
+
+
+
 }   // end namespace
 
-#endif //KEL_WORKFLOW_QUEUE_H
+#endif //KEL_WORKFLOW_QUEUES_H
--- kel_thread/kel_workflow_unittest.cpp
+++ kel_thread/kel_workflow_unittest.cpp
@@ -10,29 +10,38 @@
 namespace kel = kellerberrin;
 
 
+const size_t test_thread_count = 5;
+struct InputObject {
+
+  size_t count_;
+  std::string count_string_;
+
+};
+
+struct IntermediateObject {
+
+  size_t count_;
+  size_t in_count_;
+  std::string count_string_;
+
+};
+
+struct OutputObject {
+
+  size_t count_;
+  size_t in_count_;
+  std::string count_string_;
+
+};
+
+using InputType = std::shared_ptr<InputObject>;
+using OutputType = std::shared_ptr<OutputObject>;
+
 void unit_test_copyable() {
 
-  const size_t test_thread_count = 5;
-  struct InputObject {
 
-    size_t count_;
-    std::string count_string_;
-
-  };
-
-  struct OutputObject {
-
-    size_t count_;
-    std::string count_string_;
-
-  };
-
-  using InputType = std::shared_ptr<InputObject>;
-  using OutputType = std::shared_ptr<OutputObject>;
-
-
-  auto callback = [](InputType /*p*/, size_t /*i*/) -> OutputType { return std::make_shared<OutputObject>(); };
-  kel::WorkflowMove<InputType, OutputType> work_flow;
+  auto callback = [](size_t /*i*/, InputType /*p*/) -> OutputType { return std::make_shared<OutputObject>(); };
+  kel::WorkflowQueues<InputType, OutputType> work_flow;
   work_flow.registerProcessingFn(nullptr, nullptr, test_thread_count, callback, 7);
 
   for (size_t i = 0; i < 1000; ++i) {
@@ -41,10 +50,11 @@ void unit_test_copyable() {
 
   }
 
-  // Stop the processing threads.
-  work_flow.stopProcessing();
+  // Stop the processing threads by pushing a stop token
+  work_flow.push(nullptr);
 
   size_t out_size{0};
+  // Dequeue the output objects until the output stop token is encountered.
   OutputType out_obj = work_flow.waitAndPop();
   while(out_obj) {
 
@@ -55,6 +65,7 @@ void unit_test_copyable() {
   }
 
   kel::ExecEnv::log().info("Final - Copy - Out objects processed: {}, Out Queue Size: {}", out_size, work_flow.outputQueue().size());
+  kel::ExecEnv::log().info("Final - Copy - In Queue Size: {}", work_flow.inputQueue().size());
 
 }
 
@@ -62,50 +73,75 @@ void unit_test_copyable() {
 
 void unit_test_moveable() {
 
-  const size_t test_thread_count = 5;
-  struct InputObject {
-
-    size_t count_;
-    std::string count_string_;
-
-  };
-
-  struct OutputObject {
-
-    size_t count_;
-    std::string count_string_;
-
-  };
 
   using InputType = std::unique_ptr<InputObject>;
+  using IntermediateType = std::unique_ptr<IntermediateObject>;
   using OutputType = std::unique_ptr<OutputObject>;
 
+  std::unique_ptr<kel::WorkflowSingle<InputType>> work_flow(std::make_unique<kel::WorkflowSingle<InputType>>(nullptr));
+  std::shared_ptr<kel::WorkflowSingle<IntermediateType>> intermediate_queue(std::make_shared<kel::WorkflowSingle<IntermediateType>>(nullptr));
+  std::shared_ptr<kel::WorkflowSingle<OutputType>> output_queue(std::make_shared<kel::WorkflowSingle<OutputType>>(nullptr));
 
-  auto callback = [](InputType /*p*/, size_t /*i*/) -> OutputType { return std::make_unique<OutputObject>(); };
-  kel::WorkflowMove<InputType, OutputType> work_flow;
-  work_flow.registerProcessingFn(nullptr, nullptr, test_thread_count, callback, 7);
+  auto interqueue = [](std::shared_ptr<kel::WorkflowSingle<IntermediateType>> intermediate_queue
+      , IntermediateType intermediate_stop
+      , InputType input_stop
+      ,InputType input_item) -> void {
+
+    if (input_item == input_stop) {
+
+      intermediate_queue->push(std::move(intermediate_stop));
+
+    } else {
+
+      intermediate_queue->push(std::make_unique<IntermediateObject>());
+
+    }
+
+
+  };
+
+  auto outqueue = [](std::shared_ptr<kel::WorkflowSingle<OutputType>> output_queue
+      , OutputType output_stop
+      , IntermediateType intermediate_stop
+      , IntermediateType intermediate_item) -> void {
+
+    if (intermediate_item == intermediate_stop) {
+
+      output_queue->push(std::move(output_stop));
+
+    } else {
+
+      output_queue->push(std::make_unique<OutputObject>());
+
+    }
+
+
+  };
+
+  intermediate_queue->registerProcessingFn(test_thread_count, outqueue, output_queue, nullptr, nullptr);
+  work_flow->registerProcessingFn(test_thread_count, interqueue, std::move(intermediate_queue), nullptr, nullptr);
 
   for (size_t i = 0; i < 1000; ++i) {
 
-    work_flow.push(std::make_unique<InputObject>());
-    work_flow.push(nullptr);
+    work_flow->push(std::make_unique<InputObject>());
 
   }
 
   // Stop the processing threads.
-  work_flow.stopProcessing();
+  work_flow->push(nullptr);
 
   size_t out_size{0};
-  OutputType out_obj = work_flow.waitAndPop();
+  OutputType out_obj = output_queue->waitAndPop();
   while(out_obj) {
 
     ++out_size;
-    kel::ExecEnv::log().info("Move - Out objects processed: {}, Out Queue Size: {}", out_size, work_flow.outputQueue().size());
-    out_obj = work_flow.waitAndPop();
+    kel::ExecEnv::log().info("Move - Out objects processed: {}, Out Queue Size: {}", out_size, output_queue->inputQueue().size());
+    out_obj = output_queue->waitAndPop();
 
   }
 
-  kel::ExecEnv::log().info("Final - Move - Out objects processed: {}, Out Queue Size: {}", out_size, work_flow.outputQueue().size());
+  kel::ExecEnv::log().info("Final - Move - Out objects processed: {}, Out Queue Size: {}", out_size, output_queue->inputQueue().size());
+  kel::ExecEnv::log().info("Final - Move - In Queue Size: {}", work_flow->inputQueue().size());
 
 }
 
